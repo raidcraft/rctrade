007f0a0243fbea81dec31a7c894f636bfed19d49
Silthus <silthus@raid-craft.de>
2013-09-21 21:08:32 +0200

    initial commit

---
+++ src/main/java/de/raidcraft/trade/TradePlugin.java
@@ -0,0 +1,19 @@
+package de.raidcraft.trade;
+
+import de.raidcraft.api.BasePlugin;
+
+/**
+ * @author Silthus
+ */
+public class TradePlugin extends BasePlugin {
+
+    @Override
+    public void enable() {
+        //TODO: implement
+    }
+
+    @Override
+    public void disable() {
+        //TODO: implement
+    }
+}
