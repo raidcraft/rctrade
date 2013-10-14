fcd882b5ffc6d3c21bbbc867bc964f4051e0fb51
Philip Urban <urban.philip@googlemail.com>
2013-10-14 21:48:03 +0200

    added first trade window implementations

--- src/main/java/de/raidcraft/trade/api/PlayerTradeWindow.java
+++ src/main/java/de/raidcraft/trade/api/PlayerTradeWindow.java
@@ -1,14 +1,22 @@
 package de.raidcraft.trade.api;
 
+import de.raidcraft.RaidCraft;
+import de.raidcraft.trade.TradePlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
 
 /**
  * @author Philip Urban
  */
 public class PlayerTradeWindow extends AbstractTradeWindow implements Listener {
+
+    private static final Integer[] BLOCKED_SLOTS = {4, 13, 22, 31, 40, 49};
 
     public PlayerTradeWindow(TradePartner leftPartner, TradePartner rightPartner) {
 
@@ -21,11 +29,24 @@ public class PlayerTradeWindow extends AbstractTradeWindow implements Listener {
         inventory.setItem(31, separator.clone());
         inventory.setItem(40, separator.clone());
         inventory.setItem(49, separator.clone());
+
+        RaidCraft.getComponent(TradePlugin.class).registerEvents(this);
     }
 
     @Override
     public void open() {
 
 
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+
+        if(!event.getInventory().equals(inventory)) return;
+
+        if(Arrays.asList(BLOCKED_SLOTS).contains(event.getSlot())) {
+            event.setCancelled(true);
+            return;
+        }
     }
 }
